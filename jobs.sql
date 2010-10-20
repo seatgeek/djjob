7798f8bd805b531b16b5a7e45a9513bd94f2475d
Eric Waller <erwaller@gmail.com>
2010-10-20 18:54:33 -0400

    added the table structure

---
+++ jobs.sql
@@ -0,0 +1,11 @@
+CREATE TABLE `jobs` (
+`id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+`handler` TEXT NOT NULL,
+`queue` VARCHAR(255) NOT NULL DEFAULT 'default',
+`run_at` DATETIME NULL,
+`locked_at` DATETIME NULL,
+`locked_by` VARCHAR(255) NULL,
+`failed_at` DATETIME NULL,
+`error` TEXT NULL,
+`created_at` DATETIME NOT NULL
+) ENGINE = INNODB;
