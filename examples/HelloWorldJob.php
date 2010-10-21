6c2b2187580c8dbb8676c4a9888734588035ddb8
Eric Waller <erwaller@gmail.com>
2010-10-21 11:55:44 -0400

    added the HelloWorldJob

---
+++ examples/HelloWorldJob.php
@@ -0,0 +1,10 @@
+<?php
+
+class HelloWorldJob {
+    public function __construct($name) {
+        $this->name = $name;
+    }
+    public function perform() {
+        echo "Hello ${this->name}!\n";
+    }
+}
