8992cc43fa04a5d7f7f4b21d97346d53be2171fb
wakeless <michael@currinda.com>
2013-08-08 17:25:52 +1000

    DJJob struggles when there is unicode. Lots of "badHandlers"
    There's an issue which I've been unable to fix otherwise wherein djjob can't deserialize unicode or utf-8 characters (I'm not sure which.)
    
    This solves it by encoding the serialized data as base64.
    
    It's worth noting that this will break existing job queues as the encoding has changed.

--- DJJob.php
+++ DJJob.php
@@ -362,13 +362,22 @@ class DJJob extends DJBase {
         );
         $this->releaseLock();
     }
+        
+    protected function serialize($object) {
+      return base64_encode(serialize($object));
+    }
+
+    protected function unserialize($data) {
+      return unserialize(base64_decode($data));
+    }
+
 
     public function getHandler() {
         $rs = $this->runQuery(
             "SELECT handler FROM jobs WHERE id = ?",
             array($this->job_id)
         );
-        foreach ($rs as $r) return unserialize($r["handler"]);
+        foreach ($rs as $r) return $this->unserialize($r["handler"]);
         return false;
     }
 
@@ -384,7 +393,7 @@ class DJJob extends DJBase {
     public static function enqueue($handler, $queue = "default", $run_at = null) {
         $affected = self::runUpdate(
             "INSERT INTO jobs (handler, queue, run_at, created_at) VALUES(?, ?, ?, NOW())",
-            array(serialize($handler), (string) $queue, $run_at)
+            array($this->serialize($handler), (string) $queue, $run_at)
         );
 
         if ($affected < 1) {
@@ -401,7 +410,7 @@ class DJJob extends DJBase {
 
         $parameters = array();
         foreach ($handlers as $handler) {
-            $parameters []= serialize($handler);
+            $parameters []= $this->serialize($handler);
             $parameters []= (string) $queue;
             $parameters []= $run_at;
         }
