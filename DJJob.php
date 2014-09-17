seatgeek/djjob

commit 8c999e2c344ec8296cd51833f535c288da87653b

Added an option to fail jobs if any output is detected.  This was primarily intended for the cases where a PHP warning is printed, but no exception is thrown (like for undefined variables).  By default, jobs still succeed those cases.  After setting 'fail_on_output' to true, jobs with any output will fail with the output included in the 'error' database column.

DJJob.php
@@ -208,10 +208,11 @@ class DJWorker extends DJBase {
             "queue" => "default",
             "count" => 0,
             "sleep" => 5,
-            "max_attempts" => 5
+            "max_attempts" => 5,
+            "fail_on_output" => false
         ), $options);
-        list($this->queue, $this->count, $this->sleep, $this->max_attempts) =
-            array($options["queue"], $options["count"], $options["sleep"], $options["max_attempts"]);
+        list($this->queue, $this->count, $this->sleep, $this->max_attempts, $this->fail_on_output) =
+            array($options["queue"], $options["count"], $options["sleep"], $options["max_attempts"], $options["fail_on_output"]);
 
         list($hostname, $pid) = array(trim(`hostname`), getmypid());
         $this->name = "host::$hostname pid::$pid";
@@ -269,7 +270,8 @@ class DJWorker extends DJBase {
 
         foreach ($rs as $r) {
             $job = new DJJob($this->name, $r["id"], array(
-                "max_attempts" => $this->max_attempts
+                "max_attempts" => $this->max_attempts,
+                "fail_on_output" => $this->fail_on_output
             ));
             if ($job->acquireLock()) return $job;
         }
@@ -310,11 +312,13 @@ class DJJob extends DJBase {
 
     public function __construct($worker_name, $job_id, $options = array()) {
         $options = array_merge(array(
-            "max_attempts" => 5
+            "max_attempts" => 5,
+            "fail_on_output" => false
         ), $options);
         $this->worker_name = $worker_name;
         $this->job_id = $job_id;
         $this->max_attempts = $options["max_attempts"];
+        $this->fail_on_output = $options["fail_on_output"];
     }
 
     public function run() {
@@ -328,7 +332,21 @@ class DJJob extends DJBase {
 
         # run the handler
         try {
+
+            if($this->fail_on_output){
+                ob_start();                
+            }
+
             $handler->perform();
+
+            if($this->fail_on_output){
+                $output = ob_get_contents();
+                ob_end_clean();
+
+                if(!empty($output)){
+                    throw new Exception("Job produced unexpected output: $output");
+                }
+            }
 
             # cleanup
             $this->finish();

commit d8abd0be8e317341a777f4b4058c6f6653382eb7

Fixed a bug introduced by 8c999e2 that did not end output buffering on Exceptions (causing output printing to be delayed).

DJJob.php
@@ -353,6 +353,10 @@ class DJJob extends DJBase {
             return true;
 
         } catch (DJRetryException $e) {
+            if ($this->fail_on_output) {
+                ob_end_flush();
+            }
+            
             # attempts hasn't been incremented yet.
             $attempts = $this->getAttempts()+1;
 
@@ -368,6 +372,9 @@ class DJJob extends DJBase {
             return false;
 
         } catch (Exception $e) {
+            if ($this->fail_on_output) {
+                ob_end_flush();
+            }
 
             $this->finishWithError($e->getMessage(), $handler);
             return false;